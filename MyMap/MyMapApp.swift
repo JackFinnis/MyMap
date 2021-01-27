8047c217259e034ee781d17467c3269c7394a3b3
Jack <hoverbug@btinternet.com>
2021-01-27 16:00:49 +0000

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by Finnis on 27/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
