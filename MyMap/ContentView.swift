b83df98472148e263612183105d5f75afba7beba
Jack <hoverbug@btinternet.com>
2021-01-28 15:37:18 +0000

    Not Working

--- MyMap/ContentView.swift
+++ MyMap/ContentView.swift
@@ -6,10 +6,47 @@
 //
 
 import SwiftUI
+import CoreLocation
+import HealthKit
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Button("Start recording route") {
+            
+            startWorkout()
+        }
+    }
+    
+    func startWorkout() {
+        
+        let locationManager = CLLocationManager()
+        let store = HKHealthStore()
+        
+        // Start tracking the user.
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        // Create the route builder.
+        let routeBuilder = HKWorkoutRouteBuilder(healthStore: store, device: nil)
+    }
+    
+    // MARK: - CLLocationManagerDelegate Methods.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // Filter the raw data.
+        let filteredLocations = locations.filter { (location: CLLocation) -> Bool in
+            location.horizontalAccuracy <= 50.0
+        }
+        
+        guard !filteredLocations.isEmpty else { return }
+        
+        // Add the filtered data to the route.
+        routeBuilder.insertRouteData(filteredLocations) { (success, error) in
+            if !success {
+                // Handle any errors here.
+            }
+        }
     }
 }
